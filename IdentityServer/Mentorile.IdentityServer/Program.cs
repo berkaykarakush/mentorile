ae880c312f943271c1659f93375ae63758589abf
berkaykarakush <berkaykaraks@outlook.com>
2025-04-04 00:35:06 +0300

    Add IdentityServer project and initial setup

---
+++ IdentityServer/Mentorile.IdentityServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+using Mentorile.IdentityServer;
+using Mentorile.IdentityServer.Data;
+using Mentorile.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+
+    using var scope = builder.Build().Services.CreateScope();
+    var serviceProvider = scope.ServiceProvider;
+    var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+    applicationDbContext.Database.Migrate();
+
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    if(!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new ApplicationUser
+        {
+            UserName = "admin",
+            Email = "admin@mentorile.com",
+            EmailConfirmed = true,
+            PhoneNumber = "05554443322"
+        }, "Password12*").Wait();
+    }
+    
+
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
